98bcfb4cd907020c51196861360ab09fed339353
dhanusuya.k2022csbs@sece.ac.in <dhanusuya.k2022csbs@sece.ac.in>
2025-05-28 01:34:27 +0530

    port

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(PORT, () => {
+    app.listen(PORT,'0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
